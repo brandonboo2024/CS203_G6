a7e760147fa0b3024399fed8aa0a3a3a0dcdd2b2
AngQiLong <qilong.ang.2024@computing.smu.edu.sg>
2025-09-12 17:56:32 +0800

    This is the commit for the tariffkey folder

---
+++ src/main/java/com/example/tariffkey/TariffkeyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tariffkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TariffkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TariffkeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tariffkey/TariffkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tariffkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TariffkeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
