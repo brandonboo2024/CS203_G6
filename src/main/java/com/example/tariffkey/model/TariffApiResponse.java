1bb8c29dcef8a7da641a3a5e99cad27175d5c42b
brandonboo2024 <brandon.boo.2024@computing.smu.edu.sg>
2025-11-05 17:35:30 +0800

    added service, queries will now have 2 layers of checking
    - local db first,
    - if record doesnt exist, then make api call

--- src/main/java/com/example/tariffkey/model/TariffApiResponse.java
+++ src/main/java/com/example/tariffkey/model/TariffApiResponse.java
@@ -5,14 +5,15 @@ import lombok.*;
 import java.math.BigDecimal;
 import java.util.Map;
 
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
 public class TariffApiResponse {
 
+    private String url;
+    private int httpStatus;
+    private String body;
     private double tariffRate;
     private String[] tariffTypes;
     private Integer year;
     private String nomenclature;
-    private String url;
-    private int httpStatus;
-    private String body;
+    private boolean fromCache; // check if from cache or not
 }
